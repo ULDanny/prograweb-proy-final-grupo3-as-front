ef1de42ea4987d77e357822983b4dfdd69078ed7
francot2 <105623223+francot2@users.noreply.github.com>
2024-07-14 15:31:45 -0500

    Update FormDetallesProductos.jsx

--- src/components/FormDetallesProductos/FormDetallesProductos.jsx
+++ src/components/FormDetallesProductos/FormDetallesProductos.jsx
@@ -1,52 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import productoApi from '../../api/producto';
 
 const FormDetallesProductos = () => {
     const navigate = useNavigate();
     const { id } = useParams();
-    
+
     const [producto, setProducto] = useState({
         nombre: '',
         procesador: '',
         marca: '',
         grafica: '',
-        ram: '',
+        RAM: '',
         stock: '',
         precio: '',
-        imagen: null,
+        imagen: '',
+        vendido: 0,
+        oferta: 'no',
+        idSerie: '',
+        fecha: '',
+        createdAt: '',
+        updatedAt: ''
     });
 
     useEffect(() => {
         if (id) {
-            const productos = JSON.parse(localStorage.getItem('productos')) || [];
-            const productoEncontrado = productos.find(p => p.id.toString() === id);
-            if (productoEncontrado) {
-                setProducto(productoEncontrado);
-            }
+            fetchProducto(id);
         }
     }, [id]);
 
-    const handleImagenChange = (e) => {
-        const file = e.target.files[0];
-        setProducto({...producto, imagen: URL.createObjectURL(file)});
+    const fetchProducto = async (id) => {
+        try {
+            const productoData = await productoApi.findOne(id);
+            setProducto(productoData);
+        } catch (error) {
+            console.error('Error al cargar el producto:', error);
+        }
     };
 
-    const handleClick = () => {
-        const productos = JSON.parse(localStorage.getItem('productos')) || [];
-        if (id) {
-            const updatedProductos = productos.map(p => p.id.toString() === id ? { ...producto } : p);
-            localStorage.setItem('productos', JSON.stringify(updatedProductos));
-        } else {
-            const newProduct = {
-                id: productos.length + 1,
+    const handleClick = async () => {
+        try {
+            const payload = {
                 ...producto,
-                fechaRegistro: new Date().toLocaleDateString(),
-                estado: 'Disponible'
+                fecha: new Date().toISOString(), // Añadimos la fecha de creación o edición
             };
-            productos.push(newProduct);
-            localStorage.setItem('productos', JSON.stringify(productos));
+
+            if (id) {
+                await productoApi.update(payload);
+            } else {
+                await productoApi.create(payload);
+            }
+            navigate('/ListaProductosAdmin');
+        } catch (error) {
+            console.error('Error al guardar el producto:', error);
         }
-        navigate('/ListaProductosAdmin');
     };
 
     return (
@@ -60,13 +67,13 @@ const FormDetallesProductos = () => {
                         {producto.imagen && <img src={producto.imagen} alt="Imagen seleccionada" style={styles.imagen} />}
                     </div>
                     <div style={styles.divBotonAgregar}>
-                        <label htmlFor="inputImagen" style={styles.botonAgregarImagen}>Cargar imagen</label>
+                        <label htmlFor="inputImagen" style={styles.nombreCampo}>Link de la imagen:</label>
                         <input
-                            type="file"
+                            type="text"
                             id="inputImagen"
-                            name="inputImagen"
-                            style={{ display: 'none' }}
-                            onChange={handleImagenChange}
+                            style={styles.campo}
+                            value={producto.imagen}
+                            onChange={e => setProducto({ ...producto, imagen: e.target.value })}
                         />
                     </div>
                 </div>
@@ -120,8 +127,8 @@ const FormDetallesProductos = () => {
                                 type="text"
                                 id="ram"
                                 style={styles.campo}
-                                value={producto.ram}
-                                onChange={e => setProducto({ ...producto, ram: e.target.value })}
+                                value={producto.RAM}
+                                onChange={e => setProducto({ ...producto, RAM: e.target.value })}
                             />
                         </div>
                         <div style={styles.agregarDatos22}>
@@ -144,6 +151,26 @@ const FormDetallesProductos = () => {
                                 onChange={e => setProducto({ ...producto, precio: e.target.value })}
                             />
                         </div>
+                        <div style={styles.agregarDatos22}>
+                            <label htmlFor="oferta" style={styles.nombreCampo}>Oferta:</label>
+                            <input
+                                type="text"
+                                id="oferta"
+                                style={styles.campo}
+                                value={producto.oferta}
+                                onChange={e => setProducto({ ...producto, oferta: e.target.value })}
+                            />
+                        </div>
+                        <div style={styles.agregarDatos22}>
+                            <label htmlFor="idSerie" style={styles.nombreCampo}>ID Serie:</label>
+                            <input
+                                type="number"
+                                id="idSerie"
+                                style={styles.campo}
+                                value={producto.idSerie}
+                                onChange={e => setProducto({ ...producto, idSerie: e.target.value })}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -154,102 +181,96 @@ const FormDetallesProductos = () => {
     );
 }
 
-    const styles = {
-        section: {
-            backgroundColor: 'white',
-            width: '100%',
-            margin: '10px',
-            padding: '0px',
-            display: 'flex',
-            flexDirection: 'column',
-        },
-        tituloPagina: {
-            marginBottom: '20px',
-            width: '100%',
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            backgroundColor: 'grey',
-        },
-        titulo: {
-            margin: '15px',
-            color: 'black',
-        },
-        formAgregarProductos: {
-            display: 'flex',
-        },
-        agregarImagen: {
-            marginRight: '10px',
-            marginBottom: '0px',
-            width: '30%',
-            height: '100%',
-        },
-        imagen: {
-            width: '100%',
-            height: 'auto',
-            display: 'block',
-            marginBottom: '10px',
-            marginRight: '20px',
-            backgroundColor: 'black',
-        },
-        imagen: {
-            width: '100%',
-            height: 'auto',
-            maxHeight: '200px',
-            objectFit: 'contain',
-        },
-        botonAgregarImagen: {
-            marginLeft: '0px',
-            padding: '8px',
-            backgroundColor: 'black',
-            color: 'white',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer',
-        },
-        agregarDatos: {
-            marginBottom: '0px',
-        },
-        agregarDatos1: {
-            marginBottom: '0px',
-        },
-        agregarDatos2: {
-            marginBottom: '0px',
-            display: 'flex',
-            flexWrap: 'wrap',
-        },
-        agregarDatos22: {
-            width: '30%',
-            marginRight: '20px',
-            marginLeft: '0px',
-        },
-        nombreCampo: {
-            display: 'block',
-            marginBottom: '0px',
-            color: 'black',
-        },
-        campo: {
-            width: '100%',
-            padding: '5px',
-            borderRadius: '5px',
-            color: 'black',
-            marginBottom: '2px',
-            border: '1.5px solid black',
-            backgroundColor: 'white',
-            boxSizing: 'border-box',
-        },
-        divBotonAgregar: {
-            alignSelf: 'flex-end',
-        },
-        botonAgregar: {
-            marginLeft: '5px',
-            padding: '8px',
-            backgroundColor: 'black',
-            color: 'white',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer',
-        },
-    };
+const styles = {
+    section: {
+        backgroundColor: 'white',
+        width: '100%',
+        margin: '10px',
+        padding: '0px',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    tituloPagina: {
+        marginBottom: '20px',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: 'grey',
+    },
+    titulo: {
+        margin: '15px',
+        color: 'black',
+    },
+    formAgregarProductos: {
+        display: 'flex',
+    },
+    agregarImagen: {
+        marginRight: '10px',
+        marginBottom: '0px',
+        width: '30%',
+        height: '100%',
+    },
+    imagen: {
+        width: '100%',
+        height: 'auto',
+        display: 'block',
+        marginBottom: '10px',
+        marginRight: '20px',
+        backgroundColor: 'black',
+    },
+    nombreCampo: {
+        marginLeft: '0px',
+        padding: '8px',
+        backgroundColor: 'black',
+        color: 'white',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+    },
+    agregarDatos: {
+        marginBottom: '0px',
+    },
+    agregarDatos1: {
+        marginBottom: '0px',
+    },
+    agregarDatos2: {
+        marginBottom: '0px',
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
+    agregarDatos22: {
+        width: '30%',
+        marginRight: '20px',
+        marginLeft: '0px',
+    },
+    nombreCampo: {
+        display: 'block',
+        marginBottom: '0px',
+        color: 'black',
+    },
+    campo: {
+        width: '100%',
+        padding: '5px',
+        borderRadius: '5px',
+        color: 'black',
+        marginBottom: '2px',
+        border: '1.5px solid black',
+        backgroundColor: 'white',
+        boxSizing: 'border-box',
+    },
+    divBotonAgregar: {
+        alignSelf: 'flex-end',
+    },
+    botonAgregar: {
+        marginLeft: '5px',
+        padding: '8px',
+        backgroundColor: 'black',
+        color: 'white',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+    },
+};
 
-    export default FormDetallesProductos;
+export default FormDetallesProductos;
